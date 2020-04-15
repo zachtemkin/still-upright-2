45399fbdb4693385dfbebb1299586620668cf826
Zachary Temkin <zachtemkin@gmail.com>
2020-04-15 01:16:03 -0400

    testing some weird aesthetics

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -5,14 +5,14 @@ import Img from "gatsby-image"
 export default ( props ) => {
 
   return (
-    <article className="post">
+    <article className="post" style={{backgroundColor: props.heroImage.colors.darkMuted}}>
       <Link to={props.path}><span className="post__link" /></Link>
       <div className="post__hero-image">
         <Img fluid={props.heroImage.childImageSharp.resize}></Img>
       </div>
       <div className="post__info">
-        <h3 className="post__info__title" style={{color: props.heroImage.colors.darkVibrant}}>{props.title}</h3>
-        <p className="post__info__author-and-date">{`Post by ${props.author} \n\u2022 ${props.date}`}</p>
+        <h3 className="post__info__title" style={{color: props.heroImage.colors.lightVibrant}}>{props.title}</h3>
+        <p className="post__info__author-and-date"><b>{`Post by ${props.author}`}</b>{`\n\u2022 ${props.date}`}</p>
         <p className="post__info__category" style={{color: props.heroImage.colors.darkVibrant, backgroundColor: props.heroImage.colors.lightVibrant}}>{props.category}</p>
         <p className="post__info__excerpt">{props.excerpt}</p>
       </div>
